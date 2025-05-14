ada07a600e6dbb8c9b6cccc03173a679c4e07f65
Lerunya11 <kachanovavaleriia@gmail.com>
2025-05-14 15:12:56 +0200

    адапт

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
 (() => {
-        const menuBtn = document.querySelector('.js-open-menu');
-        const closeMenuBtn = document.querySelector('.js-close-menu');
-        const mobileMenu = document.querySelector('.js-menu-container');
-      
-        menuBtn.addEventListener('click', () => {
-          mobileMenu.classList.add('is-open');
-        });
-      
-        closeMenuBtn.addEventListener('click', () => {
-          mobileMenu.classList.remove('is-open');
-        });
-      })();
-      
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-open");
+  }
+})();
